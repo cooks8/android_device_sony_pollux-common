986dd6e0530a51911cd556fd9eb52fca1fcabead
CaHbKaUp <cahbka@gmail.com>
2017-07-28 11:33:42 +0300

    pollux-common: Add support for TWRP
    Change-Id: Ice30312fe9662370b1badcec341422ed813236a5

---
+++ board/twrp.mk
@@ -0,0 +1,5 @@
+# Recovery configurations
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
+TW_BRIGHTNESS_PATH := /sys/devices/i2c-0/0-002c/backlight/lcd-backlight/brightness
+TW_SECONDARY_BRIGHTNESS_PATH := /dev/null
+TW_THEME := landscape_hdpi
